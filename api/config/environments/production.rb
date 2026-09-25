8982332288debc3b8fae7670761a320033f428a3
Rafael Siqueira <rafaelsiqueira.dev@gmail.com>
2026-09-25 09:21:09 -0300

    build: add Docker images for Dokploy deploy

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -18,8 +18,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # config.assume_ssl = true
+  # Assume all access to the app is happening through a SSL-terminating reverse proxy
+  # (Traefik, in the Dokploy deploy).
+  config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
